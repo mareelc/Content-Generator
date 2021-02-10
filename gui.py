bd1741eea2aa9ae02c08dd7c4303ba225f0dea85
heinl11 <lchein.art@gmail.com>
2021-02-10 13:51:28 -1000

    Change to Classes

--- gui.py
+++ gui.py
@@ -2,63 +2,73 @@
 
 import tkinter as tk
 import tkinter.font as tkFont
+from open import *
 
-def gui_create():
+class Content_Generator(tk.Frame):
+    def __init__(self, root, *args, **kwargs):
+        tk.Frame.__init__(self, root, *args, **kwargs)
+        self.root = root
+        self.title_label = self.title(self.root)
+        self.user_dir = self.directions(self.root)
+        self.key1_input = tk.Entry(self.root)
+        self.key2_input = tk.Entry(self.root)
+        self.button = tk.Button(self.root, text="Generate", command=self.get)
+        self.keyword_inputs = self.keyword_input(self.root, self.key1_input, self.key2_input, self.button)
+        self.para_label = self.paragraph(self.root)
+        self.textbox = tk.Text(root, height=13, width=47)
+        self.textbox.grid(row=3, column=4, columnspan=4, rowspan=6)
+
+    def title(self, root):
+        fontStyle = tkFont.Font(size=40)
+        labelExample = tk.Label(root, text="        Content Generator         ", font=fontStyle, fg="white", bg="grey")
+        labelExample.grid(columnspan=9)
+        tk.Label(root, text=" ").grid(row=1)
+
+    def directions(self, root):
+        fontStyle2 = tkFont.Font(size=20)
+        directionsLabel = tk.Label(root, text=" Input: ", font=fontStyle2)
+        directionsLabel.grid(row=2, columnspan=2)
+        words = "Generate a paragraph of text from Wikipedia \n" \
+                "by entering two keywords below and" \
+                "\n clicking the Generate " \
+                "button. Paragraph\n " \
+                "will appear at the right of this \n" \
+                "page and will include both keywords. \n" \
+                "Results will be saved to paragraph.csv."
+        text = tk.Label(root, text=words)
+        text.grid(columnspan=4)
+
+    def keyword_input(self, root, keyword1, keyword2, button):
+        tk.Label(root, text=" ").grid(row=4)
+        tk.Label(root, text="Keyword 1").grid(row=5, column=1)
+        keyword1.grid(row=5, column=2)
+        tk.Label(root, text="Keyword 2").grid(row=6, column=1)
+        keyword2.grid(row=6, column=2)
+        tk.Label(root, text=" ").grid(row=7)
+        button.grid(columnspan=4)
+
+    def paragraph(self, root):
+        fontStyle2 = tkFont.Font(size=20)
+        tk.Label(root, text=" ").grid(column=4)
+        paragraphLabel = tk.Label(root, text="Output: ", font=fontStyle2)
+        paragraphLabel.grid(row=2, column=3, columnspan=2)
+
+    def get(self):
+        results = verify_keywords(self.key1_input.get(), self.key2_input.get())
+        # self.textbox.insert(tk.END, "Disambiguation Page Found, Type ")
+        for x in results:
+            self.textbox.insert(tk.END, x)
+            self.textbox.insert(tk.END, ", \n")
+
+def main():
+    """Main function for http_server.py."""
     root = tk.Tk()
+    app = Content_Generator(root)
     root.title("Content Generator")
     root.geometry("685x360")
     root.configure(bg="grey95")
     root.grid_columnconfigure(4, minsize=90)
-    fontStyle = tkFont.Font(size=40)
-    fontStyle2 = tkFont.Font(size=20)
-    title(root, fontStyle)
-    directions(root)
-    keyword_input(root)
-    generate_button(root)
-    paragraph(root, fontStyle2)
-    root.mainloop()
-
-def title(root, fontStyle):
-    labelExample = tk.Label(root, text="        Content Generator         ", font=fontStyle, fg="white", bg="grey")
-    labelExample.grid(columnspan=9)
-    tk.Label(root, text=" ").grid(row=1)
-
-def directions(root):
-    fontStyle2 = tkFont.Font(size=20)
-    directionsLabel = tk.Label(root, text="  Directions: ", font=fontStyle2)
-    directionsLabel.grid(row=2, columnspan=2)
-    words = "Generate a paragraph of text from Wikipedia \n" \
-            "by entering two keywords below and" \
-            "\n clicking the Generate " \
-            "button. Paragraph\n " \
-            "will appear at the right of this \n" \
-            "page and will include both keywords. \n" \
-            "Results will be saved to paragraph.csv."
-    text = tk.Label(root, text=words)
-    text.grid(columnspan=4)
-
-def keyword_input(root):
-    tk.Label(root, text=" ").grid(row=4)
-    tk.Label(root, text="Keyword 1").grid(row=5, column=1)
-    keyword1 = tk.Entry(root).grid(row=5, column=2)
-    tk.Label(root, text="Keyword 2").grid(row=6, column=1)
-    keyword2 = tk.Entry(root).grid(row=6, column=2)
-
-def generate_button(root):
-    tk.Label(root, text=" ").grid(row=7)
-    button = tk.Button(root, text="Generate")
-    button.grid(columnspan=4)
-
-def paragraph(root, fontStyle2):
-    tk.Label(root, text=" ").grid(column=4)
-    paragraphLabel = tk.Label(root, text=" Paragraph: ", font=fontStyle2)
-    paragraphLabel.grid(row=2, column=3, columnspan=2)
-    textbox = tk.Text(root, height=13, width=47)
-    textbox.grid(row=3, column=4, columnspan=4, rowspan=6)
-
-def main():
-    """Main function for http_server.py."""
-    gui_create()
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
--- open.py
+++ open.py
@@ -1,6 +1,14 @@
+# Laura Maree
 
+import wikipedia as wiki
 
-
-
-def new():
-    aasldfk
+def verify_keywords(keyword1, keyword2):
+    print("yay!")
+    verify = wiki.search(keyword1)
+    try:
+        print(wiki.page(keyword1, auto_suggest=False, redirect=True).html)
+    except wiki.exceptions.DisambiguationError as e:
+        results = list(e.options)
+        return results
+def search_wiki(keyword1, keyword2):
+    page = wiki.page(keyword1).hmtl
